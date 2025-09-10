5a14446cbc9bf2c6fa9a0189820b3cb89c223a09
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-09-10 22:44:26 +0530

    Initial Commit : Project Setup

---
+++ src/main/java/com/springproject/SmashLink/SmashLinkApplication.java
@@ -0,0 +1,14 @@
+package com.springproject.SmashLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashLinkApplication.class, args);
+        System.out.print("Hello World");
+	}
+
+}
---
+++ src/test/java/com/springproject/SmashLink/SmashLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.SmashLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
