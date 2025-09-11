9a09c4e4832f26abd4a22689df86a0835f2cbf7e
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-09-11 21:55:56 +0530

    Entity Creation

---
+++ src/main/java/com/springproject/SmashLink/entity/ShortUrl.java
@@ -0,0 +1,35 @@
+package com.springproject.SmashLink.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "short_url")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShortUrl extends AuditClass{
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String shortKey;
+
+    @Column( nullable = false)
+    private  String originalURL;
+
+    private Long clickCount;
+
+    private Boolean isPrivate;
+
+    private LocalDateTime expiresAt;
+
+}
